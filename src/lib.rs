d1c721d5d4910d68bc61e22ac77e45c0a28a6159
Marco Munizaga <marco@marcopolo.io>
2019-11-08 17:12:13 -0800

    Simplify Hello World

--- src/lib.rs
+++ src/lib.rs
@@ -1,34 +1,12 @@
-extern crate wee_alloc;
-#[global_allocator]
-static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
-
 #[macro_use]
 extern crate http_guest;
-
 use http_guest::{Request, Response};
 
-// Hello World
-pub fn server(_req: &Request<Vec<u8>>) -> Result<Response<Vec<u8>>, Box<dyn std::error::Error>> {
-    Ok(Response::builder()
+fn handler(_req: &Request<Vec<u8>>) -> Response<Vec<u8>> {
+    Response::builder()
         .status(200)
         .body(b"Hello World!".to_vec())
-        .unwrap())
+        .unwrap()
 }
 
-/// Wrapper for the server that turns the error case into a 500 response showing
-/// the error:
-fn server_(req: &Request<Vec<u8>>) -> Response<Vec<u8>> {
-    match server(req) {
-        Ok(resp) => resp,
-        Err(_) => {
-            // let body = format!("Demo Error: {:?}", e);
-            let body = "Demo Error";
-            Response::builder()
-                .status(500)
-                .body(body.as_bytes().to_owned())
-                .unwrap()
-        }
-    }
-}
-
-guest_app!(server_);
+guest_app!(handler);
